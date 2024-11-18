74271f0ae777cd977950ecda251904eef4828f42
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-18 18:50:08 +0100

    Remove : remove style generate by CLI NEXT

--- src/app/fuck/[how]/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function how({params}) {
-    return (
-        <div>fuck {params.how} </div>
-    );
-}
-
-export default how;
--- src/app/fuck/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Fuck(props) {
-    return (
-        <div>fuck</div>
-    );
-}
-
-export default Fuck;
