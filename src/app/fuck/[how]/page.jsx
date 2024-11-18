cc5a1422a87ce4ff66385bb1c653b7a9f2256cc0
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-18 12:03:31 +0100

    Add : dinamic router nextjs

---
+++ src/app/fuck/[how]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function how({params}) {
+    return (
+        <div>fuck {params.how} </div>
+    );
+}
+
+export default how;
---
+++ src/app/fuck/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Fuck(props) {
+    return (
+        <div>fuck</div>
+    );
+}
+
+export default Fuck;
