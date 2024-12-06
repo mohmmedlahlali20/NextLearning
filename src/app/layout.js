e1dcaf4c54c3face718451712fd842398fc55528
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-06 15:40:48 +0100

    fix: update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import MenuBar from "@/components/MenuBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <MenuBar/>
         {children}
       </body>
     </html>
